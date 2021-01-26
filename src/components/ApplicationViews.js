2f03cc4f5333101e099551716d30fc4236a2effe
sosinatolossa <sosinatolossa16@gmail.com>
2021-01-25 22:48:08 -0600

    added a button

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,6 +4,7 @@ import { Home } from "./Home"
 
 import { AnimalProvider } from "./animal/AnimalProvider"
 import { AnimalList } from "./animal/AnimalList"
+import { AnimalForm } from "./animal/AnimalForm"
 
 import { LocationProvider } from "./location/LocationProvider"
 import { LocationList } from "./location/LocationList"
@@ -13,6 +14,7 @@ import { EmployeeList } from "./employee/EmployeeList"
 
 import { CustomerProvider } from "./customer/CustomerProvider"
 import { CustomerList } from "./customer/CustomerList"
+
 
 export const ApplicationViews = () => {
     return (
@@ -27,8 +29,12 @@ export const ApplicationViews = () => {
             <AnimalProvider>
                 <LocationProvider>
                     <CustomerProvider>
+                        {/* Note the addition of "exact" now that we have an additional route with "/animals" in it below this Route: "/animals/create" */}
                         <Route exact path="/animals">
                             <AnimalList />
+                        </Route>
+                        <Route path="/animals/create">
+                            <AnimalForm />
                         </Route>
                     </CustomerProvider>
                 </LocationProvider>
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -3,18 +3,22 @@ import React, { useContext, useEffect } from "react"
 //To start, we need to import the context object we created in the provider component so that the Context hook can access the objects it exposes
 //The useEffect hook allows the component to reach out into the world for anything that cannot be handled during render. 
 //In this case, it is the API call for the animals.
+import { useHistory } from "react-router-dom"
 import { AnimalContext } from "./AnimalProvider"
 import { LocationContext } from "../location/LocationProvider"
 import { CustomerContext } from "../customer/CustomerProvider"
 import { AnimalCard } from "./AnimalCard"
 import "./Animal.css"
 
+
 export const AnimalList = () => {
   // This state changes when `getAnimals()` is invoked below
   const { animals, getAnimals } = useContext(AnimalContext)
-
   const { locations, getLocations } = useContext(LocationContext)
   const { customers, getCustomers } = useContext(CustomerContext)
+
+  // The useHistory hook let's us tell React which route we want to visit. We will use it to tell React to render the animal form component.
+  const history = useHistory()
 
   //useEffect - reach out to the world for something
   useEffect(() => {
@@ -27,6 +31,10 @@ export const AnimalList = () => {
 
   return (
     <div className="animals">
+      <h2>Animals</h2>
+		      <button onClick={() => {history.push("/animals/create")}}>
+            Add Animal
+          </button>
       {/* {console.log("AnimalList: Render", animals)} */}
       {
         animals.map(animal => {
